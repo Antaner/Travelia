a3d8fc3d2583d1c9bb91b8d4520986fbea22b8d4
BuildTools <unconfigured@null.spigotmc.org>
2026-03-11 15:51:54 +0100

    Initial commit

---
+++ src/main/java/net/antaner/travelia/Travelia.java
@@ -0,0 +1,18 @@
+package net.antaner.travelia;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Travelia implements ModInitializer {
+	public static final String MOD_ID = "travelia";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/antaner/travelia/TraveliaModClient.java
@@ -0,0 +1,11 @@
+package net.antaner.travelia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TraveliaModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
